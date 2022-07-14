33baf4962996bb137ea533aa0c0b814d94d462d2
gavin <gavinxu66@gmail.com>
2022-07-14 10:55:43 +0800

    HW 04 finished and all passed

--- README.md
+++ README.md
@@ -1,8 +1,16 @@
-## CS61A 
+# CS61A 
+
+[![Berkerly - CS61a Spring 2021](https://img.shields.io/badge/Berkerly-CS61a_Spring_2021-blue?style=for-the-badge)](https://)
+[![Status - Finishing](https://img.shields.io/badge/Status-Finishing-red?style=for-the-badge)](https://)
+
+[![Lab - 01-05, 10](https://img.shields.io/badge/Lab-01--05%2C_10-success?style=for-the-badge)](https://)
+[![Homework - 01-04](https://img.shields.io/badge/Homework-01--04-success?style=for-the-badge)](https://)
+[![Project - 01-02](https://img.shields.io/badge/Lab-01--02-success?style=for-the-badge)](https://)
 
 Spring 2021, Pamela Fox, Paul Hilfinger 
 
 ---
+
 ### Lab
 - [x] Lab 00: Getting Started
 - [x] Lab 01: Variables & Functions, Control
@@ -10,13 +18,21 @@ Spring 2021, Pamela Fox, Paul Hilfinger
 - [x] Lab 03: Recursion, Tree Recursion
 - [x] Lab 04: Midterm Review (No assignment available)
 - [x] Lab 05: Python Lists, Data Abstraction, Trees
-  
-  ...
+- [ ] Lab 06:
+- [ ] Lab 075:
+- [ ] Lab 08:
+- [ ] Lab 09:
 - [x] Lab 10: Scheme
+
+---
+
 ### Homework
 - [x] HW 01: Variables & Functions, Control
 - [x] HW 02: Higher-Order Functions
 - [x] HW 03: Recursion
+- [x] HW 04: Trees, Data Abstraction
+
+---
 
 ### Project
 - [x] Hog
--- hws/hw04/hw04.py
+++ hws/hw04/hw04.py
@@ -1,3 +1,5 @@
+
+
 HW_SOURCE_FILE = __file__
 
 
@@ -52,13 +54,13 @@ def planet(size):
     """Construct a planet of some size."""
     assert size > 0
     "*** YOUR CODE HERE ***"
-
+    return ['planet', size]
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
     "*** YOUR CODE HERE ***"
-
+    return w[1]
 
 def is_planet(w):
     """Whether w is a planet."""
@@ -118,9 +120,29 @@ def balanced(m):
     True
     """
     "*** YOUR CODE HERE ***"
+    # def mobile_torque(arm1, arm2):
+    #     torque1 = length(arm1) * total_weight(end(arm1))
+    #     torque2 = length(arm2) * total_weight(end(arm2))
+    #     return True if torque1 == torque2 else False
+    
+    # def balanced_object(obj):
+    #     if is_planet(obj):
+    #         return True
+    #     if is_mobile(obj):
+    #         if is_planet(end(left(obj))) and is_planet(end(right(obj))):
+    #             return mobile_torque(left(obj), right(obj))
+    #         else:
+    #             return balanced_object(end(left(obj))) and balanced_object(end(right(obj))) \
+    #                  and mobile_torque(left(obj), right(obj))
+
+    # return balanced_object(m)
+    if is_planet(m):
+        return True
+    l, r = end(left(m)), end(right(m))
+    return balanced(l) and balanced(r) and total_weight(l) * length(left(m)) == total_weight(r) * length(right(m))
 
 
-def totals_tree(m):
+def totals_tree(m): 
     """Return a tree representing the mobile with its total weight at the root.
 
     >>> t, u, v = examples()
@@ -150,7 +172,12 @@ def totals_tree(m):
     True
     """
     "*** YOUR CODE HERE ***"
-
+    if is_planet(m):
+        return tree(size(m))
+    else:
+        l, r = end(left(m)), end(right(m))
+        return tree(total_weight(m), [totals_tree(l), totals_tree(r)]) 
+    
 
 def replace_thor_at_leaf(t, thors_replacement):
     """Returns a new tree where every leaf value equal to "thor" has
@@ -182,7 +209,19 @@ def replace_thor_at_leaf(t, thors_replacement):
     True
     """
     "*** YOUR CODE HERE ***"
+    new_t = copy_tree(t)
 
+    # can modify the new_t
+    def helper(new_t):
+        if is_leaf(new_t):
+            if label(new_t) == 'thor':
+                new_t[0] = thors_replacement
+        else:
+            for branch in branches(new_t):
+                helper(branch)
+        return new_t
+
+    return helper(new_t)
 
 def has_path(t, word):
     """Return whether there is a path in a tree where the entries along the path
@@ -216,8 +255,11 @@ def has_path(t, word):
     """
     assert len(word) > 0, 'no path for empty word.'
     "*** YOUR CODE HERE ***"
+    if len(word) == 1:
+        return word[0] == label(t)
+    return label(t) == word[0] and any([has_path(b, word[1:]) for b in branches(t)])
 
-
+            
 def preorder(t):
     """Return a list of the entries in this tree in the order that they
     would be visited by a preorder traversal (see problem description).
@@ -229,7 +271,18 @@ def preorder(t):
     [2, 4, 6]
     """
     "*** YOUR CODE HERE ***"
+    ls = list()
 
+    def helper(t, ls):
+        if is_leaf(t):
+            return ls.append(label(t))
+        else:
+            ls.append(label(t))
+            for branch in branches(t):
+                helper(branch, ls)
+        return ls
+    
+    return helper(t, ls)
 
 def interval(a, b):
     """Construct an interval from a to b."""
@@ -239,12 +292,12 @@ def interval(a, b):
 def lower_bound(x):
     """Return the lower bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[0]
 
 def upper_bound(x):
     """Return the upper bound of interval x."""
     "*** YOUR CODE HERE ***"
-
+    return x[1]
 
 def str_interval(x):
     """Return a string representation of interval x.
@@ -263,17 +316,18 @@ def add_interval(x, y):
 def mul_interval(x, y):
     """Return the interval that contains the product of any value in x and any
     value in y."""
-    p1 = x[0] * y[0]
-    p2 = x[0] * y[1]
-    p3 = x[1] * y[0]
-    p4 = x[1] * y[1]
-    return [min(p1, p2, p3, p4), max(p1, p2, p3, p4)]
+    p1 = lower_bound(x) * lower_bound(y)
+    p2 = lower_bound(x) * upper_bound(y)
+    p3 = upper_bound(x) * lower_bound(y)
+    p4 = upper_bound(x) * upper_bound(y)
+    return interval(min(p1, p2, p3, p4), max(p1, p2, p3, p4))
 
 
 def sub_interval(x, y):
     """Return the interval that contains the difference between any value in x
     and any value in y."""
     "*** YOUR CODE HERE ***"
+    return interval(lower_bound(x) - upper_bound(y), upper_bound(x) - lower_bound(y))
 
 
 def div_interval(x, y):
@@ -281,6 +335,8 @@ def div_interval(x, y):
     any value in y. Division is implemented as the multiplication of x by the
     reciprocal of y."""
     "*** YOUR CODE HERE ***"
+    assert(upper_bound(y) * lower_bound(y) > 0)
+
     reciprocal_y = interval(1 / upper_bound(y), 1 / lower_bound(y))
     return mul_interval(x, reciprocal_y)
 
@@ -305,8 +361,8 @@ def check_par():
     >>> lower_bound(x) != lower_bound(y) or upper_bound(x) != upper_bound(y)
     True
     """
-    r1 = interval(1, 1)  # Replace this line!
-    r2 = interval(1, 1)  # Replace this line!
+    r1 = interval(1, 100000)  # Replace this line!
+    r2 = interval(1, 100000)  # Replace this line!
     return r1, r2
 
 
@@ -324,7 +380,19 @@ def quadratic(x, a, b, c):
     '0 to 10'
     """
     "*** YOUR CODE HERE ***"
-
+    def f(t):
+        return a*t*t + b*t +c
+    x1, x2 = lower_bound(x), upper_bound(x)
+    f1, f2 = f(x1), f(x2)
+    s = -b/(2*a)
+    minimum = f(s)
+    if x1 <= s <= x2:
+        if a > 0:
+            return interval(minimum, max(f1, f2))
+        else:
+            return interval(min(f1, f2), minimum)
+    else:
+        return interval(min(f1, f2), max(f1, f2))
 
 # Tree ADT
 
